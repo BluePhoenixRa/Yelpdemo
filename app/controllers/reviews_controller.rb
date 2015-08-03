874f919336a321bed7bc4f192ce3ce24740d3a9c
BluePhoenixRa <r.arefin@gmail.com>
2015-08-03 15:13:12 -0400

    Set user permissions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,12 +2,12 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: [:edit, :update, :destroy]
   before_action :set_restaurant
   before_action :authenticate_user!
+  before_action :check_user, only: [:edit, :update, :destroy]
 
   respond_to :html
 
   def new
     @review = Review.new
-    respond_with(@review)
   end
 
   def edit
@@ -36,7 +36,10 @@ class ReviewsController < ApplicationController
 
   def destroy
     @review.destroy
-    respond_with(@review)
+    respond_to do |format|
+      format.html { redirect_to restaurant_path(@restaurant), notice: 'Review was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
@@ -46,6 +49,12 @@ class ReviewsController < ApplicationController
     
     def set_restaurant
       @restaurant = Restaurant.find(params[:restaurant_id])
+    end
+    
+    def check_user
+      unless (@review.user == current_user) || (current_user.admin?)
+        redirect_to root_url, alert: "Sorry, this review belongs to someone else"
+      end
     end
 
     def review_params
